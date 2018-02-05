03dc86b8de7cc7f3dc8df638212e1f783d7075af
Sam Jones <sam.jones494@googlemail.com>
2018-02-05 22:19:37 +0000

    extracts repeated test code to test helper method

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'entering names' do
   it 'allows two players to enter their names' do
-    visit('/')
-    fill_in 'player_1', with: 'Sam'
-    fill_in 'player_2', with: 'Milo'
-    click_button 'Play!'
+    sign_in_and_play
     expect(page).to have_content('Sam vs. Milo')
   end
 end
--- spec/features/player_2_hitpoints_spec.rb
+++ spec/features/player_2_hitpoints_spec.rb
@@ -1,9 +1,6 @@
 feature 'player 2 hitpoints' do
   scenario "player 2's hitpoints are displayed on the play screen" do
-    visit('/')
-    fill_in 'player_1', with: 'Sam'
-    fill_in 'player_2', with: 'Milo'
-    click_button 'Play!'
+    sign_in_and_play
     expect(page).to have_content('Milo: 50hp')
   end
 end
